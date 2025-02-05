ebae4746f98618b35886cfa245c3344f3425ef3e
moizawan07 <moizahmedawan321@gmail.com>
2025-02-05 00:12:49 -0800

    update

--- index.js
+++ index.js
@@ -3,11 +3,41 @@ function clicked(e) {
     display.value += e;
 }
 function calculate() {
+    var characters = [];
     var inputValue = document.getElementById("display");
-    console.log(inputValue.value);
-    inputValue.value = eval(inputValue.value);
+    if (inputValue.value) {
+        for (var i = 0; i < inputValue.value.length; i++) {
+            characters.push(inputValue.value[i]);
+        }
+        console.log(characters);
+        switch (characters[0] && characters[characters.length - 1]) {
+            case '/':
+                inputValue.value = 'Error..';
+                inputValue.classList.add('Error');
+                break;
+            case '*':
+                inputValue.value = 'Error..';
+                inputValue.classList.add('Error');
+                break;
+            case '-':
+                inputValue.value = 'Error..';
+                inputValue.classList.add('Error');
+                break;
+            case '+':
+                inputValue.value = 'Error..';
+                inputValue.classList.add('Error');
+                break;
+            default: inputValue.value = eval(inputValue.value);
+        }
+    }
+    else {
+        inputValue.value = 'Error..';
+        inputValue.classList.add('Error');
+        console.log(inputValue);
+    }
 }
 function clearAll() {
     var inputValue = document.getElementById("display");
     inputValue.value = "";
+    inputValue.classList.remove('Error');
 }
